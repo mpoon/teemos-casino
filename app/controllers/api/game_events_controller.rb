ab722b8c243beaed54fa487192486f12aff4d290
mpoon <mpoon@twitch.tv>
2013-12-05 02:37:50 -0800

    add api key

--- app/controllers/api/game_events_controller.rb
+++ app/controllers/api/game_events_controller.rb
@@ -1,9 +1,11 @@
 class Api::GameEventsController < Api::BaseController
+  before_action :require_api_key
+
   def start
     event = GameEvent.create(
       game_id: params.require(:game_id),
       kind: "game_start",
-      expires_at: DateTime.now + GameEvent::EXPIRY 
+      expires_at: DateTime.now + GameEvent::EXPIRY
     )
 
     if event.errors.empty?
@@ -29,6 +31,14 @@ class Api::GameEventsController < Api::BaseController
       render json: {message: "success"}
     else
       render json: {error: event.errors.first}
+    end
+  end
+
+  private
+
+  def require_api_key
+    if headers['Authorization'] != TeemosCasino::Application.config.api_key
+      render json: {error: "Missing or wrong API key"}, status: :forbidden
     end
   end
 end
