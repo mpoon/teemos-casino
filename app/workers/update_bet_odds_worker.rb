df6feb5de7b98a0c854dcc3ead96b9cf1abf59d6
mpoon <mpoon@twitch.tv>
2013-12-08 22:14:51 -0800

    clean up code

--- app/workers/update_bet_odds_worker.rb
+++ app/workers/update_bet_odds_worker.rb
@@ -1,24 +1,28 @@
 class UpdateBetOddsWorker
   include Sidekiq::Worker
 
+  UPDATE_INTERVAL = 1
+
   # Update bet odds to client
   def perform(event_id)
     event = GameEvent.find(event_id)
     odds = event.bet_odds
 
     while Time.now() < event.expires_at
-      new_odds = event.bet_odds
-      if odds == new_odds
+      odds = event.bet_odds
+      previous_odds ||= odds
+
+      if odds == previous_odds
         logger.info "[UpdateBetOddsWorker] Bet odds did not change"
       else
-        odds = new_odds
+        previous_odds = odds
         logger.info "[UpdateBetOddsWorker] Game id: #{event.game_id} Odds blue: #{odds[:blue]} purple: #{odds[:purple]}"
         PusherClient.global('bet_odds', {
           blue: odds[:blue],
           purple: odds[:purple]
           })
       end
-      sleep TeemosCasino::Application.config.bet_odds_update_interval
+      sleep UPDATE_INTERVAL
     end
   end
 end
--- config/initializers/bet_odds.rb
+++
@@ -1,1 +0,0 @@
-TeemosCasino::Application.config.bet_odds_update_interval = 1
