791998b28f0038def4fe80c1c84b56bfc552dc73
mpoon <mpoon@twitch.tv>
2013-12-08 21:37:47 -0800

    don't send pusher when bet odds don't change

--- app/workers/update_bet_odds_worker.rb
+++ app/workers/update_bet_odds_worker.rb
@@ -4,14 +4,19 @@ class UpdateBetOddsWorker
   # Update bet odds to client
   def perform(event_id)
     event = GameEvent.find(event_id)
+    odds = event.bet_odds
 
     while Time.now() < event.expires_at
-      odds = event.bet_odds
-      logger.info "[UpdateBetOddsWorker] Game id: #{event.game_id} Odds blue: #{odds[:blue]} purple: #{odds[:purple]}"
-      PusherClient.global('bet_odds', {
-        blue: odds[:blue],
-        purple: odds[:purple]
-        })
+      if event.bet_odds == odds
+        logger.info "[UpdateBetOddsWorker] Bet odds did not change"
+      else
+        odds = event.bet_odds
+        logger.info "[UpdateBetOddsWorker] Game id: #{event.game_id} Odds blue: #{odds[:blue]} purple: #{odds[:purple]}"
+        PusherClient.global('bet_odds', {
+          blue: odds[:blue],
+          purple: odds[:purple]
+          })
+      end
       sleep TeemosCasino::Application.config.bet_odds_update_interval
     end
   end
