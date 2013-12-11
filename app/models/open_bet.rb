1e1bc24801985a3bf537166d2ee535d1c98c2444
Hebo <doubletime@gmail.com>
2013-12-10 20:49:02 -0800

    Round betting odds to 1 or 0 if possible

--- app/models/open_bet.rb
+++ app/models/open_bet.rb
@@ -32,13 +32,19 @@ class OpenBet < ActiveRecord::Base
     if bluePool == 0
       blueMult = 0
     else
-      blueMult = (pool / bluePool).round(2)
+      blueMult = (pool / bluePool).round(1)
+      # Round to integer if the number is the floating point equivalent
+      # (1.0 -> 1)
+      blueMult = blueMult.to_i if blueMult.to_i == blueMult
     end
 
     if purplePool == 0
       purpleMult = 0
     else
-      purpleMult = (pool / purplePool).round(2)
+      purpleMult = (pool / purplePool).round(1)
+      # Round to integer if the number is the floating point equivalent
+      # (1.0 -> 1)
+      purpleMult = purpleMult.to_i if purpleMult.to_i == purpleMult
     end
 
     return {blue: blueMult, purple: purpleMult} # blue, purple
--- test/models/open_bet_test.rb
+++ test/models/open_bet_test.rb
@@ -7,13 +7,13 @@ class OpenBetTest < ActiveSupport::TestCase
     bet1 = bets(:one)
     bet2 = bets(:two)
 
-    expected_odds = {blue: 9.33, purple: 1.12}
+    expected_odds = {blue: 9.3, purple: 1.1}
     assert_equal expected_odds, open_bet.odds
 
     user = users(:bettor_three)
     bet3 = Bet.create(open_bet: open_bet, team: 'blue', amount: 2000, user_id: user.id)
 
-    expected_odds = {blue: 1.06, purple: 17.12}
+    expected_odds = {blue: 1.1, purple: 17.1}
     assert_equal expected_odds, open_bet.odds
   end
 
@@ -21,6 +21,15 @@ class OpenBetTest < ActiveSupport::TestCase
     open_bet = open_bets(:no_bets)
 
     expected_odds = {blue: 0, purple: 0}
+    assert_equal expected_odds, open_bet.odds
+  end
+
+  test "should return integers if equivalent" do
+    open_bet = open_bets(:no_bets)
+    user = users(:bettor_three)
+    bet = Bet.create(open_bet: open_bet, team: 'blue', amount: 10, user_id: user.id)
+
+    expected_odds = {blue: 1, purple: 0}
     assert_equal expected_odds, open_bet.odds
   end
 end
