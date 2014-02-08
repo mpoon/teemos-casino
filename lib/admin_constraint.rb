daef009a661db627ba14a07ec8ba765955a560f8
Michael L Poon <michaelpoon@Michaels-MacBook-Pro.local>
2014-02-07 23:46:03 -0800

    turn off admin requirement for dev environment

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,5 +1,7 @@
 class AdminConstraint
   def matches?(request)
+    return true if Rails.env == 'development'
+
     return false unless request.session[:_current_user_id]
     User.find(request.session[:_current_user_id]).admin?
   end
