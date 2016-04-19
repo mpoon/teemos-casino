bb66e6ee7fc54583f793e49bf115d891655bfe86
Manuel van Rijn <manuel@manuelvanrijn.nl>
2016-04-19 22:09:34 +0200

    I changed my domain :)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 # Sentry/raven sidekiq integration
 require 'raven/sidekiq'
 
-# http://manuel.manuelles.nl/sidekiq-heroku-redis-calc/
+# http://manuelvanrijn.nl/sidekiq-heroku-redis-calc/
 # https://github.com/mperham/sidekiq/issues/117
 
 Sidekiq.configure_client do |config|
